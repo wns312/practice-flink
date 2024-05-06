03e9c813520c616b48f3c101529b57653c24921d
wns312 <wns312@gmail.com>
2024-05-06 14:15:23 +0900

    chore: Add flink dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,38 @@
 plugins {
     kotlin("jvm") version "1.9.23"
     application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 group = "jyk"
 version = "1.0-SNAPSHOT"
 
+val flinkVersion = "1.19.0"
+
 repositories {
     mavenCentral()
 }
 
+
 dependencies {
+    implementation("org.apache.flink:flink-streaming-java:$flinkVersion")
+//    implementation("org.apache.flink:flink-java:$flinkVersion")
+//    implementation("org.apache.flink:flink-connector-kafka:${flinkVersion}")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
     testImplementation(kotlin("test"))
+//    testImplementation("org.apache.flink:flink-test-utils:$flinkVersion")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.slf4j:slf4j-simple:2.0.12")
+
 }
 
 tasks.test {
@@ -19,7 +40,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(17)
 }
 
 application {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,13 @@
+
+import org.apache.flink.api.common.typeinfo.Types
+import org.apache.flink.connector.datagen.source.DataGeneratorSource
+import org.apache.flink.connector.datagen.source.GeneratorFunction
+
+
 fun main(args: Array<String>) {
+    val generatorFunction = GeneratorFunction<Long, String> { index -> "Number: $index" }
+    val numberOfRecords= 1000L
+    val source = DataGeneratorSource(generatorFunction, numberOfRecords, Types.STRING)
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
